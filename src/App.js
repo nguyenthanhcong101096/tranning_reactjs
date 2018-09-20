0cff05d13e00bc9f6ed83155b83c9bf82dff4f1a
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-09-20 22:12:12 +0700

    deloy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Velcome ahihi React</h1>
+        </header>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import bugsnag from 'bugsnag-js';
+import createPlugin from 'bugsnag-react';
+const bugsnagClient = bugsnag('20890bfa4578112f7670584de38e0459')
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
+
+bugsnagClient.notify(new Error('HELLO REACTJS TRACK BUGS'))
+
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
+registerServiceWorker();
