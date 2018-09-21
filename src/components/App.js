50046889468cbb1e457bbbd5af758b50c98eb937
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-09-21 21:39:48 +0700

    refs

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,33 @@ import logo from '../assets/logo.svg';
 import '../styles/App.css';
 import Main from './settingColor/main';
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       type: 'settingColor'
     }
   }
-  
-  renderView(){
-    if(this.state.type == 'settingColor')
-      return <Main/>
+
+  renderView() {
+    if (this.state.type == 'settingColor')
+      return <Main />
   }
-  
+
+  change() {
+    this.setState({ type: this.refs.type.value })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Velcome ahihi React</h1>
+          <input
+            ref='type'
+            type='text'
+            placeholder='Nhập spring' />
+          <button onClick={() => { this.change() }}>Chuyển</button>
           {this.renderView()}
         </header>
       </div>
